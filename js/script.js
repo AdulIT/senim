1f5eaf6d671180c7e83d0b3b87b698d3bbcfa4c0
AdulIT <spilberg.max@bk.ru>
2024-02-01 10:58:42 +0600

    init&feat: start project, three sections for desktop

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const sliderContainer = document.querySelector('.clients__slider')
+const sliderCopy = sliderContainer.querySelector('.clients__slider-slide').cloneNode(true)
+sliderContainer.appendChild(sliderCopy)
+
+const dropdownIcon = document.querySelector('.menu__arrow-down')
+const dropdownMenu = document.querySelector('.dropdown')
+
+dropdownIcon.addEventListener('click', () =>
+{
+    dropdownMenu.classList.toggle('show')
+})
